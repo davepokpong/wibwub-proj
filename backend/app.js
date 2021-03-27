d6d5ba737854b7be73b594e23f8b5aeb0c65c9e2
davepokpong <davepokpong@gmail.com>
2021-03-27 17:09:46 +0700

    backend

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ status : 'Running!' });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Application is running on port 3000');
+});
